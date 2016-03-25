20b0f328e3e5fb5a4a1cc82416727eb3e8899688
ABur29 <askar@MacBook-Pro-Askar.local>
2016-03-25 16:57:51 +1000

    Add sample

---
+++ SampleAnalyze.py
@@ -0,0 +1,6 @@
+a = {"Temperature": 1, "Light": 2, "Wind": 3, "Pressure": 4, "Voltage": 5, "Team": 6, "Date": "пролпро", "TTT": 6}
+b = {"Temperature": 8, "Light": 9, "Wind": 10, "Pressure": 11, "Voltage": 12, "Team": 13, "Date": "hghg", "TTT": 6}
+c = {"Temperature": 8, "Light": 9, "Wind": 10, "Pressure": 11, "Voltage": 12, "Team": 13, "Date": "trtese", "TTT": 6}
+z = [a,b,c] #массив словарей
+asd = FindMean(z)
+print(asd)
